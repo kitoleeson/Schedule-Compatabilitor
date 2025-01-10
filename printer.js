74c3d1d973fe73170f109abd391cecd79afe5782
Kito Lee Son <kleeson@ualberta.ca>
2025-01-10 12:37:00 -0700

    starting off

---
+++ printer.js
@@ -0,0 +1,3 @@
+function printSchedule() {
+    print('');
+}
---
+++ schema.js
@@ -0,0 +1,56 @@
+{
+    name: 'kito';
+    year: 2;
+    program: 'computer science';
+    classes: [
+        {
+            name: 'WRS 104',
+            type: 'LEC',
+            days: 'MWF',
+            time: '14:00',
+            location: 'T 1-104',
+        },
+        {
+            name: 'CMPUT 200',
+            type: 'LEC',
+            days: 'TH',
+            time: '11:00',
+            location: 'TL B-002',
+        },
+        {
+            name: 'CMPUT 200',
+            type: 'LAB',
+            days: 'T',
+            time: '17:00',
+            location: 'CCIS L1-160',
+        },
+        {
+            name: 'CMPUT 201',
+            type: 'LEC',
+            days: 'MWF',
+            time: '13:00',
+            location: 'C E1-60',
+        },
+        {
+            name: 'CMPUT 201',
+            type: 'LAB',
+            days: 'H',
+            time: '14:00',
+            location: 'CSC 1-59',
+        },
+        {
+            name: 'MUSIC 156',
+            type: 'LEC',
+            days: 'TH',
+            time: '8:00',
+            location: 'FAB 2-020',
+        },
+        {
+            name: 'MUSIC 156',
+            type: 'SEM',
+            days: 'F',
+            time: '9:00',
+            location: 'FAB 2-028',
+        },
+    ];
+}
